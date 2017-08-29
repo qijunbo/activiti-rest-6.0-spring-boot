528a1e422126cf20b0e7fe6cf35e18e585b94bc8
LAPTOP-MTIQEU28\qijunbo <junboqi@foxmail.com>
2017-08-29 10:41:06 +0800

    add  servlet confiuration.

--- src/main/java/com/example/activiti/conf/ActivitiEngineConfiguration.java
+++ src/main/java/com/example/activiti/conf/ActivitiEngineConfiguration.java
@@ -17,13 +17,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-
 public class ActivitiEngineConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(ActivitiEngineConfiguration.class);
