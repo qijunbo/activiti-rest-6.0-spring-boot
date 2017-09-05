f376390b8bdb9e6fa433854bb6a9f18029e6bc56
qijunbo <junboqi@foxmail.com>
2017-09-05 17:45:53 +0800

    make it be able to run in  war model.

--- src/main/java/com/sunway/activiti/ActivitiRestApp.java
+++ src/main/java/com/sunway/activiti/ActivitiRestApp.java
@@ -1,45 +1,20 @@
 package com.sunway.activiti;
 
-import java.util.List;
-
-import org.activiti.engine.TaskService;
-import org.activiti.engine.task.Task;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ActivitiRestApp {
+public class ActivitiRestApp extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ActivitiRestApp.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ActivitiRestApp.class);
-    }
-
-    @Autowired
-    private TaskService taskService;
-
-    // @Bean
-    public CommandLineRunner activitiDemo() {
-        return (args) -> {
-            taskService.createTaskQuery().list();
-            while (true) {
-                List<Task> tasks = taskService.createTaskQuery().taskCandidateGroup("managers").list();
-                System.out.println("Active outstanding tasks: [" + tasks.size() + "]");
-                for (int i = 0; i < tasks.size(); i++) {
-                    Task task = tasks.get(i);
-                    System.out.println("Processing Task [" + task.getName() + "]");
-
-                }
-
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        };
     }
 
 }
